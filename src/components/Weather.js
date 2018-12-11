7eb1f541b03a7d568c3f8ef591b846b600045002
Akash <aprajapati05@gmail.com>
2018-12-11 13:16:07 +0530

    added weather result component

---
+++ src/components/Weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Weather extends React.Component{
+    render(){
+        return(
+            <div>
+                Show Result here
+            </div>
+        );
+    }
+}
+
+export default Weather;
