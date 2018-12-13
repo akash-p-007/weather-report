6df3ed6770656f98c45bfd5dcf17aa11afae836d
Akash <aprajapati05@gmail.com>
2018-12-13 18:23:28 +0530

    Integrated google suggestion to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,54 @@
+import PropTypes from "prop-types"
 import React from "react";
+import GoogleMapLoader from "react-google-maps-loader"
+import GooglePlacesSuggest from "react-google-places-suggest"
 
-const Form = (props) => (
-          <form onSubmit={props.getWeather}>
-            <input type="text" name="city" placeholder="
-            Enter your city" />
-            <button>Fetch</button>
-          </form>       
+const MY_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
+class Form extends React.Component{
+  state = {
+    search: "",
+    value: "",
+  }
 
-);
+  handleInputChange = e => {
+    this.setState({search: e.target.value, value: e.target.value})
+  }
+
+  handleSelectSuggest = (geocodedPrediction, originalPrediction) => {
+    console.log(geocodedPrediction, originalPrediction)
+    this.setState({search: "", value: geocodedPrediction.formatted_address})
+  }
+  render(){
+
+    const {search, value} = this.state
+    
+    return(
+      <GoogleMapLoader
+        params={{
+          key: MY_API_KEY,
+          libraries: "places,geocode",
+        }}
+        render={googleMaps =>
+        googleMaps && (
+        <GooglePlacesSuggest
+          googleMaps={googleMaps} 
+          autocompletionRequest={{
+              input: search,
+          }}
+          onSelectSuggest={this.handleSelectSuggest}>
+
+        <form onSubmit={this.props.getWeather}>
+          <input type="text" name="city" placeholder="
+          Enter your city" value={value} onChange={this.handleInputChange.bind(this)} />
+          <button>Fetch</button>
+        </form>  
+        </GooglePlacesSuggest>
+        )
+      }
+      />
+    );
+  }
+}
+
 export default Form;
